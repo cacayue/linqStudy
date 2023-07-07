461bfe407c694ecdfe37f864d60227c16176e039
ma futain <a15453606@outlook.com>
2023-07-07 14:08:01 +0800

    init expression treee

---
+++ expressiontree01/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Linq.Expressions;
+
+LoopExpression loop = Expression.Loop(
+    Expression.Call(null,
+        typeof(Console).GetMethod("WriteLine", new Type[]{ typeof(string) })!, 
+        Expression.Constant(("Hello")))
+);
+    
+BlockExpression block = Expression.Block(loop);
+
+Expression<Action> lambdaExpression = Expression.Lambda<Action>(block);
+
+lambdaExpression.Compile().Invoke();
